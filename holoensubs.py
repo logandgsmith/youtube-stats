ba6c2e26ddfd8a31819b2d68a510666a3bce94fd
Logan Smith <loganda.smith@ufl.edu>
2020-09-28 00:57:39 -0400

    Added flags, prepping for cron

--- holoensubs.py
+++ holoensubs.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+__version__ = 'youtube-stats v0.2.0'
+
+import argparse
 import config
 import csv
 import requests
@@ -7,8 +10,18 @@ import requests
 from datetime import datetime
 from pytz import timezone
 
+class Group:
+    """Represent a group for comparison"""
+    def __init__(self, name, members=None):
+        self.name = name
+
+        if members is None:
+            self.members = []
+        else:
+            self.members = members
+
 class Member:
-    """Represents a member and her page, subs, etc"""
+    """Represents a member of a group's page, subs, etc"""
     def __init__(self, name, chan_id):
         self.name = name
         self.chan_id = chan_id
@@ -33,10 +46,31 @@ class Member:
         else:
             return int(res.json()['items'][0]['statistics']['subscriberCount'])
 
+def show_rankings(group):
+    """Sorts the group and prints their ranking"""
+    group.members.sort(reverse=True)
+
+    print(f'Current Sub Count Rankings for {group.name}:')
+
+    for member in group.members:
+        if member.subs < 1000000:
+            print(f'{member.name} with {member.subs // 1000}K subs')
+        else:
+            print(f'{member.name} with {member.subs // 1000000}M subs')
+
 def main():
     tz = timezone('US/Eastern')
 
-    holo_myth = [
+    # Parse Arguments
+    parser = argparse.ArgumentParser(description='Record and analyze information about groups of YouTubers')
+    parser.add_help = True
+    parser.add_argument('-r', '--rankings', action='store_true', help='Display rankings on screen')
+    parser.add_argument('-n', '--no-logging', action='store_true', help='Don\'t log to file')
+    parser.add_argument('--version', action='version', version=__version__)
+    flags = parser.parse_args()
+
+    # Create a group from the HoloEN Members
+    holo_myth_members = [
         Member('Amelia Watson', 'UCyl1z3jo3XHR1riLFKG5UAg'),
         Member('Calliope Mori', 'UCL_qhgtOy0dy1Agp8vkySQg'),
         Member('Gura Gawr', 'UCoSrY_IQQVpmIRZ9Xf-y93g'),
@@ -44,19 +78,19 @@ def main():
         Member('Kiara Takanashi', 'UCHsx4Hqa-1ORjQTh9TYDhww')
     ]
 
-    with open('holo-en-stats.csv', 'a', newline='') as statsfile:
-        writer = csv.writer(statsfile)
-        date = [datetime.now(tz)]
-        writer.writerow(date + [member.subs for member in holo_myth])
+    holo_myth = Group('HoloLive Myth', holo_myth_members)
 
-    holo_myth.sort(reverse=True)
+    # Display ranks in order if needed
+    if flags.rankings:
+        show_rankings(holo_myth)
 
-    print('Current Sub Count Rankings for Hololive Myth:')
-    for member in holo_myth:
-        print(f'{member.name} with {member.subs // 1000}K subs')
+    # Write to a .csv file
+    if flags.no_logging:
+       with open('holo-en-stats.csv', 'a', newline='') as log_file:
+           writer = csv.writer(log_file)
+           date = [datetime.now(tz)]
+           writer.writerow(date + [member.subs for member in holo_myth.members])
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
